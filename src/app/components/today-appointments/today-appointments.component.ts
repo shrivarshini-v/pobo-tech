45f9b0576afbc2f6c7966c5807f3e28d1e3ba844
Shrivarshini <84311980+shrivarshini-v@users.noreply.github.com>
2025-02-23 23:42:28 +0530

    Updated Code with responsive (#2)

--- src/app/components/today-appointments/today-appointments.component.ts
+++ src/app/components/today-appointments/today-appointments.component.ts
@@ -17,46 +17,5 @@ interface Appointment {
   imports:[CommonModule],
 })
 export class TodayAppointmentsComponent {
-  appointments = [
-    {
-      id: 487,
-      name: 'Sanjai',
-      email: 'sanjaikumar.pobo@gmail.com',
-      phone: '9655770823',
-      date: '21-FEB-2025 12:00 PM',
-      type: 'Consultation - Direct Visit',
-      paid: true,
-      status: 'Booked'
-    },
-    {
-      id: 485,
-      name: 'Joshva',
-      email: 'joshva.pobo@gmail.com',
-      phone: '9965480147',
-      date: '21-FEB-2025 1:00 PM',
-      type: 'Consultation - Direct Visit',
-      paid: true,
-      status: 'In Progress'
-    }
-  ];
 
-  acceptAppointment(id: number) {
-    console.log(`Accepted appointment #${id}`);
-  }
-
-  rejectAppointment(id: number) {
-    console.log(`Rejected appointment #${id}`);
-  }
-
-  markMissed(id: number) {
-    console.log(`Marked missed appointment #${id}`);
-  }
-
-  openNow(id: number) {
-    console.log(`Opening appointment #${id}`);
-  }
-
-  completeAppointment(id: number) {
-    console.log(`Completed appointment #${id}`);
-  }
 }
